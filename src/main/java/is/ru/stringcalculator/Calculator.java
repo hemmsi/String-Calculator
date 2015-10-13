5e6c3882164b255a9066d3098c089b00a30cae49
hemmsi <hermanningi@gmail.com>
2015-10-13 01:34:52 +0000

    Calculator correctly handles input with a delimiter selected by the user

--- src/main/java/is/ru/stringcalculator/Calculator.java
+++ src/main/java/is/ru/stringcalculator/Calculator.java
@@ -3,24 +3,38 @@ package is.ru.stringcalculator;
 public class Calculator {
 
     public static int add(String text){
-				
-		String[] numbers = split(text);
-		
-		int total = 0;
-		for(int i = 0; i < numbers.length; i++){
-			total += toInt(numbers[i]);
-		}
-		
-		return total;
+        String[] numbers;
+        if(text.length() > 2 && text.charAt(0) == '/' && text.charAt(1) == '/'){
+            String delimiter = String.valueOf(text.charAt(2));
+            numbers = splitDiffDel(text.substring(3), delimiter);
+
+        }else{
+            numbers = split(text, "");
+        }
+
+        int total = 0;
+        for(int i = 0; i < numbers.length; i++){
+            total += toInt(numbers[i]);
+        }
+
+        return total;
     }
 
     private static int toInt(String text){
-    	if(text.equals("")){
-			return 0;
-		}
-    	return Integer.parseInt(text);
+        if(text.equals("")){
+            return 0;
+        }
+        return Integer.parseInt(text);
     }
-    private static String[] split(String text){
-    	return text.split("\\n|,");
-    } 
+    private static String[] split(String text, String delimiter){
+
+        return text.split("\n|,|" + delimiter);
+    }
+    private static String[] splitDiffDel(String text, String delimiter){
+      return split(text,delimiter);
+
+    }
+    public static void main(String[] args){
+       System.out.println(add("1,5"));
+    }
 }
